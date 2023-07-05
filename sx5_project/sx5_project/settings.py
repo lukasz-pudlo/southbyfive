141a75d684d439f45d75c1069663f0f5ca3d33b8
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-07-05 23:45:34 +0100

    Added basic user login functionality using Django's built-in authentication system

--- sx5_project/sx5_project/settings.py
+++ sx5_project/sx5_project/settings.py
@@ -142,3 +142,5 @@ LOCATION_FIELD = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LOGIN_REDIRECT_URL = '/races'
--- sx5_project/sx5_project/urls.py
+++ sx5_project/sx5_project/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('races/', include(('races.urls', 'races'), namespace='races'))
+    path('races/', include(('races.urls', 'races'), namespace='races')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
