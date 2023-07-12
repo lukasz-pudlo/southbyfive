77cc8b91bedfce4f5abeecfb32f3542019d80023
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-07-13 00:51:53 +0100

    Create Runner and Result objects based on data in uploaded Excel file

--- sx5_project/races/views.py
+++ sx5_project/races/views.py
@@ -1,6 +1,14 @@
+import os
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import get_list_or_404
+import pandas as pd
+from django.db import transaction
 from typing import Any, Dict
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from pathlib import Path
+
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from races.models import Race, Result, Runner
@@ -28,6 +36,44 @@ class RaceCreateView(CreateView):
     model = Race
     template_name = 'races/race_form.html'
     form_class = RaceForm
+
+    # This ensures that all database operations in this block are executed as a single transaction
+    @transaction.atomic
+    def form_valid(self, form):
+        self.object = form.save()
+        if self.request.FILES:
+            excel_file = self.request.FILES['race_file']
+            fs = FileSystemStorage()
+            filename = fs.save(excel_file.name, excel_file)
+            uploaded_file_url = fs.url(filename)
+
+            uploaded_file_path = os.path.join(settings.MEDIA_ROOT, filename)
+
+            df = pd.read_excel(uploaded_file_path)
+
+            for index, row in df.iterrows():
+                # Here, 'First Name', 'Middle Name', 'Last Name', 'Category', and 'Time' are the column names in the Excel file.
+                first_name = row['First Name']
+                middle_name = row['Middle Name']
+                last_name = row['Last Name']
+                category = row['Category']
+                # Convert time string to a pandas.Timedelta object.
+                time = pd.to_timedelta(row['Time'])
+
+                runner, created = Runner.objects.get_or_create(
+                    first_name=first_name,
+                    middle_name=middle_name,
+                    last_name=last_name,
+                    category=category
+                )
+
+                Result.objects.create(
+                    race=self.object,
+                    runner=runner,
+                    time=time
+                )
+
+        return super().form_valid(form)
 
 
 class RaceUpdateView(UpdateView):
