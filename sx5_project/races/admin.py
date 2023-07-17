521aebc145781527c3c21b475fe80782c34f6e15
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-07-17 10:31:47 +0100

    Define list display for Race, Runner, and Result models

--- sx5_project/races/admin.py
+++ sx5_project/races/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
 
 from races.models import Race, Runner, Result
-from races.forms import ResultForm
+from races.forms import ResultForm, RaceForm
 
 
 class ResultAdmin(admin.ModelAdmin):
     form = ResultForm
     readonly_fields = ['time']
+    list_display = ('id', 'race', 'runner', 'time',
+                    'general_position', 'gender_position', 'category_position')
 
 
-models = [Race, Runner]
-admin_classes = {Result: ResultAdmin}
+class RaceAdmin(admin.ModelAdmin):
+    form = RaceForm
+    list_display = ('id', 'name', 'race_date', 'race_number')
 
-for model in models:
-    admin.site.register(model)
+
+class RunnerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'middle_name', 'last_name', 'category')
+
+
+models = [Race, Runner, Result]
+admin_classes = {Race: RaceAdmin, Runner: RunnerAdmin, Result: ResultAdmin}
 
 for model, admin_class in admin_classes.items():
     admin.site.register(model, admin_class)
