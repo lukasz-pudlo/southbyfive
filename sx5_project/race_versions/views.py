7da548f8aaca534e65c8a626f1aa3f1fff276685
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-09-02 13:01:55 +0100

    Display ResultVersion object detail in a template

--- sx5_project/race_versions/views.py
+++ sx5_project/race_versions/views.py
@@ -16,5 +16,6 @@ class RaceVersionDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['result_versions'] = self.object.result_versions.all()
+        context['result_versions'] = self.object.race_versions_result_versions.all()
+
         return context
