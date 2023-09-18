d9c4c7d4bfb71b68931c89df00f18a0dbd8d444e
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-09-18 23:20:01 +0100

    Remove re_path from the main urls

--- sx5_project/sx5_project/urls.py
+++ sx5_project/sx5_project/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^races/?', include(('races.urls', 'races'), namespace='races')),
+    path('races/', include(('races.urls', 'races'), namespace='races')),
     path('race-versions/', include(('race_versions.urls',
          'race-versions'),  namespace='race-versions')),
+    path('classifications/', include(('classifications.urls',
+         'classifications'), namespace='classifications')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
