87b10b490635308cecc6c2bf7753a4328f4ff3a9
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-09-18 23:18:31 +0100

    Remove re_path from races urls

--- sx5_project/races/urls.py
+++ sx5_project/races/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-from django.urls import re_path
 from races.views import RaceListView, RaceDetailView, RaceCreateView, RaceUpdateView, RaceDeleteView
 
 app_name = 'races'
 
 urlpatterns = [
-    re_path(r'^$', RaceListView.as_view(), name='list'),
-    re_path(r'^race/(?P<pk>\d+)/?$', RaceDetailView.as_view(), name='detail'),
-    re_path(r'^race/new/?$', RaceCreateView.as_view(), name='new'),
-    re_path(r'^race/(?P<pk>\d+)/edit/?$',
-            RaceUpdateView.as_view(), name='edit'),
-    re_path(r'^race/(?P<pk>\d+)/delete/?$',
-            RaceDeleteView.as_view(), name='delete')
+    path('', RaceListView.as_view(), name='list'),
+    path('race/<int:pk>/', RaceDetailView.as_view(), name='detail'),
+    path('race/new/', RaceCreateView.as_view(), name='new'),
+    path('race/<int:pk>/edit/', RaceUpdateView.as_view(), name='edit'),
+    path('race/<int:pk>/delete/',
+         RaceDeleteView.as_view(), name='delete')
 ]
