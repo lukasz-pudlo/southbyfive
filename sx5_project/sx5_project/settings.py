d68c379a4cf1cf510233b067104e798a3b3c2f11
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-10-20 22:41:39 +0100

    Add instructions on project set-up to README

--- sx5_project/sx5_project/settings.py
+++ sx5_project/sx5_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8839-2-98-203-64.ngrok-free.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
