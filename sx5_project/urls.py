8df6869d8822aed3090003fa61b924f444e43f55
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-11-01 21:05:36 +0000

    Redirect to races when you navigate to root url

--- sx5_project/urls.py
+++ sx5_project/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='races/', permanent=False)),
     path('races/', include(('races.urls', 'races'), namespace='races')),
     path('race-versions/', include(('race_versions.urls',
          'race-versions'),  namespace='race-versions')),
