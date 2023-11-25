498f8db5a9ac8a2aba32a4a4a85da44ba9445eaa
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-11-25 23:48:12 +0000

    Add classification calculation explanation to info page

--- sx5_project/settings.py
+++ sx5_project/settings.py
@@ -147,44 +147,18 @@ WSGI_APPLICATION = 'sx5_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-# if 'RDS_DB_NAME' in os.environ:
-#     # Running on AWS
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv['RDS_DB_NAME'],
-#             'USER': os.getenv['RDS_USERNAME'],
-#             'PASSWORD': os.getenv['RDS_PASSWORD'],
-#             'HOST': os.getenv['RDS_HOSTNAME'],
-#             'PORT': os.getenv['RDS_PORT'],
-#         }
-#     }
-# else:
-#     # Running locally
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv("POSTGRES_DB"),
-#             'USER': os.getenv("POSTGRES_USER"),
-#             'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-#             'HOST': os.getenv("POSTGRES_HOST"),
-#             'PORT': os.getenv("POSTGRES_PORT"),
-#         }
-#     }
-
 # Database configuration
-rds_credentials = get_rds_credentials()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': rds_credentials.get('NAME'),
-        'USER': rds_credentials.get('USER'),
-        'PASSWORD': rds_credentials.get('PASSWORD'),
-        'HOST': rds_credentials.get('HOST'),
-        'PORT': rds_credentials.get('PORT'),
-    }
-}
+# rds_credentials = get_rds_credentials()
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': rds_credentials.get('NAME'),
+#         'USER': rds_credentials.get('USER'),
+#         'PASSWORD': rds_credentials.get('PASSWORD'),
+#         'HOST': rds_credentials.get('HOST'),
+#         'PORT': rds_credentials.get('PORT'),
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -197,31 +171,42 @@ DATABASES = {
 #     }
 # }
 
-# S3 configuration
-s3_credentials = get_s3_credentials()
-AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DEV_POSTGRES_DB'),
+        'USER': os.getenv('DEV_POSTGRES_USER'),
+        'PASSWORD': os.getenv('DEV_POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DEV_POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
 
-# Static files (CSS, JavaScript, images)
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# # S3 configuration
+# s3_credentials = get_s3_credentials()
+# AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
+# AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# Media files (uploads)
-AWS_MEDIA_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
+# # Static files (CSS, JavaScript, images)
+# AWS_STATIC_LOCATION = 'static'
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # Use local storage for static and media files
-# STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# # Media files (uploads)
+# AWS_MEDIA_LOCATION = 'media'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
-# # Ensure STATICFILES_STORAGE is set to the default
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# Use local storage for static and media files
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Ensure STATICFILES_STORAGE is set to the default
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 # Password validation
