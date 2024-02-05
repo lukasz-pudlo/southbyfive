3abffa9be2d359b2893f567d8e878d90ed6d369b
Lukasz Pudlo <l.pudlo1990@gmail.com>
2024-02-05 23:12:26 +0000

    Do not display nan when runner category is missing

--- sx5_project/settings.py
+++ sx5_project/settings.py
@@ -81,7 +81,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['southbyfive.run', 'www.southbyfive.run', 'localhost', '127.0.0.1',
                  '.southbyfive.run', 'southbyfive.eu-west-1.elasticbeanstalk.com', 'http://southbyfive-app-env.eba-vma6ktp3.eu-west-1.elasticbeanstalk.com', '.ngrok-free.app']
@@ -148,18 +148,18 @@ WSGI_APPLICATION = 'sx5_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# # Database configuration for deployment to AWS Elastic Beanstalk.
-# rds_credentials = get_rds_credentials()
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': rds_credentials.get('NAME'),
-#         'USER': rds_credentials.get('USER'),
-#         'PASSWORD': rds_credentials.get('PASSWORD'),
-#         'HOST': rds_credentials.get('HOST'),
-#         'PORT': rds_credentials.get('PORT'),
-#     }
-# }
+# Database configuration for deployment to AWS Elastic Beanstalk.
+rds_credentials = get_rds_credentials()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': rds_credentials.get('NAME'),
+        'USER': rds_credentials.get('USER'),
+        'PASSWORD': rds_credentials.get('PASSWORD'),
+        'HOST': rds_credentials.get('HOST'),
+        'PORT': rds_credentials.get('PORT'),
+    }
+}
 
 # # Database configuration for local development, but with access to RDS database.
 # DATABASES = {
@@ -173,35 +173,35 @@ WSGI_APPLICATION = 'sx5_project.wsgi.application'
 #     }
 # }
 
-# Database configuration for local development, with local database.
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DEV_POSTGRES_DB'),
-        'USER': os.getenv('DEV_POSTGRES_USER'),
-        'PASSWORD': os.getenv('DEV_POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DEV_POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    }
-}
+# # Database configuration for local development, with local database.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DEV_POSTGRES_DB'),
+#         'USER': os.getenv('DEV_POSTGRES_USER'),
+#         'PASSWORD': os.getenv('DEV_POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('DEV_POSTGRES_HOST'),
+#         'PORT': os.getenv('POSTGRES_PORT'),
+#     }
+# }
 
-# # S3 bucket configuration for deployment to AWS Elastic Beanstalk.
-# s3_credentials = get_s3_credentials()
-# AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
-# AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# S3 bucket configuration for deployment to AWS Elastic Beanstalk.
+s3_credentials = get_s3_credentials()
+AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# # Static files (CSS, JavaScript, images)
-# AWS_STATIC_LOCATION = 'static'
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# Static files (CSS, JavaScript, images)
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # Media files (uploads)
-# AWS_MEDIA_LOCATION = 'media'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
+# Media files (uploads)
+AWS_MEDIA_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
 # # S3 bucket configuration for local development but with access to AWS S3 bucket.
 # AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -220,12 +220,12 @@ DATABASES = {
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
-# Local static and media configuration for local development.
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# Ensure STATICFILES_STORAGE is set to the default
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# # Local static and media configuration for local development.
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# # Ensure STATICFILES_STORAGE is set to the default
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 # Password validation
