155f8937c4c985256ee72ac135b62485e243941f
Lukasz Pudlo <l.pudlo1990@gmail.com>
2024-10-31 23:18:32 +0000

    Set up local development with Docker

--- sx5_project/settings.py
+++ sx5_project/settings.py
@@ -148,18 +148,18 @@ WSGI_APPLICATION = 'sx5_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# Database configuration for deployment to AWS Elastic Beanstalk.
-rds_credentials = get_rds_credentials()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': rds_credentials.get('NAME'),
-        'USER': rds_credentials.get('USER'),
-        'PASSWORD': rds_credentials.get('PASSWORD'),
-        'HOST': rds_credentials.get('HOST'),
-        'PORT': rds_credentials.get('PORT'),
-    }
-}
+# # Database configuration for deployment to AWS Elastic Beanstalk.
+# rds_credentials = get_rds_credentials()
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': rds_credentials.get('NAME'),
+#         'USER': rds_credentials.get('USER'),
+#         'PASSWORD': rds_credentials.get('PASSWORD'),
+#         'HOST': rds_credentials.get('HOST'),
+#         'PORT': rds_credentials.get('PORT'),
+#     }
+# }
 
 # # Database configuration for local development, but with access to RDS database.
 # DATABASES = {
@@ -173,41 +173,24 @@ DATABASES = {
 #     }
 # }
 
-# # Database configuration for local development, with local database.
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DEV_POSTGRES_DB'),
-#         'USER': os.getenv('DEV_POSTGRES_USER'),
-#         'PASSWORD': os.getenv('DEV_POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('DEV_POSTGRES_HOST'),
-#         'PORT': os.getenv('POSTGRES_PORT'),
-#     }
-# }
+# Database configuration for local development, with local database.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DEV_POSTGRES_DB'),
+        'USER': os.getenv('DEV_POSTGRES_USER'),
+        'PASSWORD': os.getenv('DEV_POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DEV_POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
 
-# S3 bucket configuration for deployment to AWS Elastic Beanstalk.
-s3_credentials = get_s3_credentials()
-AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-# Static files (CSS, JavaScript, images)
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# Media files (uploads)
-AWS_MEDIA_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
-
-# # S3 bucket configuration for local development but with access to AWS S3 bucket.
-# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-# AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+# # S3 bucket configuration for deployment to AWS Elastic Beanstalk.
+# s3_credentials = get_s3_credentials()
+# AWS_ACCESS_KEY_ID = s3_credentials.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = s3_credentials.get("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = s3_credentials.get("AWS_STORAGE_BUCKET_NAME")
+# AWS_S3_REGION_NAME = s3_credentials.get("AWS_S3_REGION_NAME")
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # # Static files (CSS, JavaScript, images)
@@ -219,6 +202,23 @@ MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 # AWS_MEDIA_LOCATION = 'media'
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
+
+# S3 bucket configuration for local development but with access to AWS S3 bucket.
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Static files (CSS, JavaScript, images)
+AWS_STATIC_LOCATION = 'static'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files (uploads)
+AWS_MEDIA_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
 # # Local static and media configuration for local development.
 # STATIC_URL = '/static/'
@@ -272,3 +272,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_REDIRECT_URL = '/races'
+
+
+print("Database name:", os.getenv('DEV_POSTGRES_DB'))
+print("User:", os.getenv('DEV_POSTGRES_USER'))
+print("Password:", os.getenv('DEV_POSTGRES_PASSWORD'))
+print("Host:", os.getenv('DEV_POSTGRES_HOST'))
+print("Port:", os.getenv('POSTGRES_PORT'))
