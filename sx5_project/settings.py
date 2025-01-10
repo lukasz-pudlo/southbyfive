3d14e683edbdfed1ac16c0b0088046eda0c1a87d
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-01-10 20:54:11 +0000

    Add whitenoise to settings

--- sx5_project/settings.py
+++ sx5_project/settings.py
@@ -109,6 +109,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -173,6 +174,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # environmnet = os.getenv('ENVIRONMENT')
