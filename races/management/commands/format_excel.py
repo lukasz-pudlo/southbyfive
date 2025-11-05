0aa879216f0ac8042212d4cff1b69ac5e2b5ba6c
Lukasz Pudlo <l.pudlo1990@gmail.com>
2025-11-05 00:41:50 +0000

    Rename columns and start reformatting the time

--- races/management/commands/format_excel.py
+++ races/management/commands/format_excel.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime
 
 
 class Command(BaseCommand):
     help = "Formats the supplied Excel file"
 
     def add_arguments(self, parser):
-        parser.add_argument("filenames", nargs="+", help="List of files to process")
+        parser.add_argument("filenames", nargs="+",
+                            help="List of files to process")
 
     def handle(self, *args, **options):
         for filename in options["filenames"]:
@@ -15,6 +17,13 @@ class Command(BaseCommand):
                 df = pd.read_excel(
                     f"{settings.BASE_DIR.parent}/races/race_files/{filename}.xlsx"
                 )
+                print(
+                    f"The types of data of {filename} are as follows: {df.dtypes}")
+                df = df.rename(columns={"Number": "Paricipant Number",
+                                        "Forename": "First Name", "Surname": "Last Name", "Age_on_Race_Day": "Category"})
                 print(f"The import Excel data as pandas dataframe: {df}")
+                df['Time'] = df['Time'].dt.strftime('%H:%M:%S')
+                print(f"The df after time formatting: {df}")
             except Exception as e:
-                raise CommandError(f"There was an error processing {filename}: {e}")
+                raise CommandError(
+                    f"There was an error processing {filename}: {e}")
