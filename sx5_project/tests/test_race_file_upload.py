4f03b5da2127ff735222d2253d52b6f008327060
Lukasz Pudlo <l.pudlo1990@gmail.com>
2026-01-02 23:54:29 +0000

    Start writing test to verify classification recalculation

--- sx5_project/tests/test_race_file_upload.py
+++ sx5_project/tests/test_race_file_upload.py
@@ -8,6 +8,7 @@ from django.test import TestCase, override_settings
 from django.contrib.auth.models import User
 
 
+from classifications.models import Classification, ClassificationResult
 from races.models import Race, Result, Runner, Season
 
 
@@ -28,83 +29,6 @@ def generate_file_with_runners():
 
     uploaded_file = SimpleUploadedFile(
         "test_kings.xlsx",
-        excel_file.read(),
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
-    return uploaded_file
-
-
-def generate_kings():
-    wb = openpyxl.Workbook()
-    ws = wb.active
-
-    ws.append(
-        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
-    )
-    ws.append(["Lukasz", "Pudlo", "121", "MS", "Unaffiliated", "00:19:31"])
-    ws.append(["Callum", "Wallace", "654", "MS",
-              "Bellahouston Harriers", "00:20:51"])
-    ws.append(["Richard", "Cooper", "654", "MS",
-              "Cambuslang Harriers", "00:21:30"])
-
-    excel_file = io.BytesIO()
-    wb.save(excel_file)
-    excel_file.seek(0)
-
-    uploaded_file = SimpleUploadedFile(
-        "test_kings.xlsx",
-        excel_file.read(),
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
-    return uploaded_file
-
-
-def generate_linn():
-    wb = openpyxl.Workbook()
-    ws = wb.active
-
-    ws.append(
-        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
-    )
-    ws.append(["Callum", "Wallace", "654", "MS",
-              "Bellahouston Harriers", "00:20:51"])
-    ws.append(["Lukasz", "Pudlo", "121", "MS", "Unaffiliated", "00:19:31"])
-    ws.append(["Richard", "Cooper", "654", "MS",
-              "Cambuslang Harriers", "00:21:30"])
-
-    excel_file = io.BytesIO()
-    wb.save(excel_file)
-    excel_file.seek(0)
-
-    uploaded_file = SimpleUploadedFile(
-        "test_linn.xlsx",
-        excel_file.read(),
-        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
-    return uploaded_file
-
-
-def generate_rouken():
-    wb = openpyxl.Workbook()
-    ws = wb.active
-
-    ws.append(
-        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
-    )
-    ws.append(["Callum", "Wallace", "654", "MS",
-              "Bellahouston Harriers", "00:20:51"])
-    ws.append(["Lukasz", "Pudlo", "121", "MS", "Unaffiliated", "00:19:31"])
-    ws.append(["Richard", "Cooper", "654", "MS",
-              "Cambuslang Harriers", "00:21:30"])
-    ws.append(["Magda", "Thomas", "654", "MS",
-              "Bellahouston Harriers", "00:24:21"])
-
-    excel_file = io.BytesIO()
-    wb.save(excel_file)
-    excel_file.seek(0)
-
-    uploaded_file = SimpleUploadedFile(
-        "test_rouken.xlsx",
         excel_file.read(),
         content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
@@ -433,12 +357,119 @@ class TestFieldCorrectness(TestCase):
         )
 
 
-class TestFieldCorrectness(TestCase):
+def generate_kings():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.append(
+        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
+    )
+    ws.append(["Andrew", "Anderson", "727", "M40",
+              "Cambuslang Harriers", "00:18:59"])
+    ws.append(["Callum", "Wallace", "811", "MS",
+              "Bellahouston Harriers", "00:19:07"])
+    ws.append(["Ethan", "Tyler", "814", "MS",
+              "Garscube Harriers", "00:19:15"])
+    ws.append(["Richard", "Cooper", "734", "MS",
+              "Cambuslang Harriers", "00:19:42"])
+
+    excel_file = io.BytesIO()
+    wb.save(excel_file)
+    excel_file.seek(0)
+
+    uploaded_file = SimpleUploadedFile(
+        "test_kings.xlsx",
+        excel_file.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    return uploaded_file
+
+
+def generate_linn():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.append(
+        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
+    )
+    ws.append(["Finlay", "Murray", "892", "MS",
+              "East Sutherland Athletics Club", "00:23:39"])
+    ws.append(["Ethan", "Tyler", "814", "MS",
+              "Garscube Harriers", "00:24:36"])
+    ws.append(["Callum", "Wallace", "811", "MS",
+              "Bellahouston Harriers", "00:24:44"])
+    ws.append(["Richard", "Cooper", "734", "MS",
+              "Cambuslang Harriers", "00:25:15"])
+
+    excel_file = io.BytesIO()
+    wb.save(excel_file)
+    excel_file.seek(0)
+
+    uploaded_file = SimpleUploadedFile(
+        "test_linn.xlsx",
+        excel_file.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    return uploaded_file
+
+
+def generate_rouken():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.append(
+        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
+    )
+    ws.append(["Andrew", "Anderson", "727", "M40",
+              "Cambuslang Harriers", "00:21:00"])
+    ws.append(["Callum", "Wallace", "811", "MS",
+              "Bellahouston Harriers", "00:21:03"])
+    ws.append(["Richard", "Cooper", "734", "MS",
+              "Cambuslang Harriers", "00:21:28"])
+
+    excel_file = io.BytesIO()
+    wb.save(excel_file)
+    excel_file.seek(0)
+
+    uploaded_file = SimpleUploadedFile(
+        "test_rouken.xlsx",
+        excel_file.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    return uploaded_file
+
+
+def generate_pollok():
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.append(
+        ["First Name", "Last Name", "Participant Number", "Category", "Club", "Time"]
+    )
+    ws.append(["Andrew", "Anderson", "727", "M40",
+              "Cambuslang Harriers", "00:21:00"])
+    ws.append(["Callum", "Wallace", "811", "MS",
+              "Bellahouston Harriers", "00:21:03"])
+    ws.append(["Richard", "Cooper", "734", "MS",
+              "Cambuslang Harriers", "00:21:28"])
+
+    excel_file = io.BytesIO()
+    wb.save(excel_file)
+    excel_file.seek(0)
+
+    uploaded_file = SimpleUploadedFile(
+        "test_rouken.xlsx",
+        excel_file.read(),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+    return uploaded_file
+
+
+class TestClassificationRecalculation(TestCase):
     maxDiff = None
 
     @classmethod
     def setUpTestData(cls):
-        cls.season = Season.objects.create(season_start_year=2024)
         cls.season = Season.objects.create(season_start_year=2025)
         cls.user = User.objects.create_user(
             username="testuser", password="testpass123")
@@ -446,19 +477,13 @@ class TestFieldCorrectness(TestCase):
     def setUp(self):
         self.client.login(username="testuser", password="testpass123")
 
-        # Create a race based on true result data
-        file_path = f"{settings.BASE_DIR}/tests/files/2025/rouken.xlsx"
-        with open(file_path, "rb") as f:
-            uploaded_file = SimpleUploadedFile(
-                "rouken.xlsx",
-                f.read(),
-                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            )
+        # Add King's Park race
+        kings_file = generate_kings()
 
         form_data = {
-            "name": "Test Rouken Glen",
+            "name": "KP Classification Recalculation Test",
             "season_start_year": 2025,
-            "race_file": uploaded_file,
+            "race_file": kings_file,
         }
 
         # Post to the URL: /races/race/new/
@@ -469,55 +494,92 @@ class TestFieldCorrectness(TestCase):
         # Check that the race was created
         self.assertEqual(Race.objects.count(), 1)
 
-    # Test that the values are the same in the file and in the result table
-    def test_values_are_the_same(self):
-        file_path = f"{settings.BASE_DIR}/tests/files/2025/rouken.xlsx"
+        # Add Linn Park race
+        linn_file = generate_linn()
 
-        df = pd.read_excel(file_path, dtype=str)
-        df = df.fillna("")
-        first_name_list_df = df["First Name"].values.tolist()
-        last_name_list_df = df["Last Name"].values.tolist()
-        participant_number_list_df = df["Participant Number"].values.tolist()
-        category_list_df = df["Category"].values.tolist()
-        time_list_df = df["Time"].values.tolist()
+        form_data = {
+            "name": "LP Classification Recalculation Test",
+            "season_start_year": 2025,
+            "race_file": linn_file,
+        }
 
-        # Replace empty strings in the Excel with "Unaffiliated"
-        df.loc[df["Club"] == "", "Club"] = "Unaffiliated"
-        club_list_df = df["Club"].values.tolist()
+        # Post to the URL: /races/race/new/
+        response = self.client.post("/races/race/new/", form_data)
 
-        first_name_list_app = []
-        last_name_list_app = []
-        participant_number_list_app = []
-        category_list_app = []
-        club_list_app = []
-        time_list_app = []
+        self.assertEqual(response.status_code, 302)
 
-        results = Result.objects.filter(race=1)
-        for result in results:
-            first_name_list_app.append(result.runner.first_name)
-            last_name_list_app.append(result.runner.last_name)
-            participant_number_list_app.append(
-                result.runner.participant_number)
-            category_list_app.append(result.runner.category)
-            club_list_app.append(result.runner.club)
-            time_list_app.append(result.time)
+        # Check that the race was created
+        self.assertEqual(Race.objects.count(), 2)
 
+        # Add Rouken Glen race
+        rouken_file = generate_rouken()
+
+        form_data = {
+            "name": "RG Classification Recalculation Test",
+            "season_start_year": 2025,
+            "race_file": rouken_file,
+        }
+
+        # Post to the URL: /races/race/new/
+        response = self.client.post("/races/race/new/", form_data)
+
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the race was created
+        self.assertEqual(Race.objects.count(), 3)
+
+        # Add Pollok Park race
+        pollok_file = generate_pollok()
+
+        form_data = {
+            "name": "PP Classification Recalculation Test",
+            "season_start_year": 2025,
+            "race_file": pollok_file,
+        }
+
+        # Post to the URL: /races/race/new/
+        response = self.client.post("/races/race/new/", form_data)
+
+        self.assertEqual(response.status_code, 302)
+
+        # Check that the race was created
+        self.assertEqual(Race.objects.count(), 4)
+
+    def test_nr_of_points_is_correct(self):
+        # Get the classification after Rouken Glen race
+        classification_after_rg = Classification.objects.get(
+            slug="kp-classification-recalculation-test-2025-classification")
+        callum_runner_object = Runner.objects.get(
+            first_name="Callum", last_name="Wallace")
+        callum_classification_result_after_rg = ClassificationResult.objects.get(
+            runner=callum_runner_object, classification=classification_after_rg)
+
+        overall_nr_of_races = Race.objects.all().count()
+        self.assertEqual(overall_nr_of_races, 3,
+                         "The number of races is not 3")
+
+        # Check that Callum Wallace is in the classification results after Rouken Glen
         self.assertEqual(
-            first_name_list_df, first_name_list_app, "The " "first names are different"
-        )
-        self.assertEqual(
-            last_name_list_df, last_name_list_app, "The " "last names are different"
-        )
-        self.assertEqual(
-            participant_number_list_df,
-            participant_number_list_app,
-            "The " "participant numbers " "names are different",
-        )
-        self.assertEqual(
-            category_list_df, category_list_app, "The " "category are different"
-        )
-        self.assertEqual(club_list_df, club_list_app,
-                         "The " "clubs are different")
-        self.assertEqual(
-            time_list_df, time_list_app, "The " "time result are different"
-        )
+            callum_classification_result_after_rg.runner.participant_number, "811", "Lukasz's participant number is incorrect")
+
+        self.assertEqual(callum_classification_result_after_rg.general_points,
+                         4, "Callum Wallace has an incorrect number of points after Rouken Glen")
+
+    def test_runner_out_of_series(self):
+        pollok_classification = Classification.objects.get(
+            slug="pp-classification-recalculation-test-2025-classification")
+        pollok_classification_runner_count = ClassificationResult.objects.filter(
+            classification=pollok_classification).count()
+        self.assertEqual(pollok_classification_runner_count, 3,
+                         "There should be only 3 runners in the classification after Pollok Park")
+        # Check that the correct runner was removed from the classification
+        callum_runner_object = Runner.objects.get(participant_number="811")
+        andrew_runner_object = Runner.objects.get(participant_number="727")
+        richard_runner_object = Runner.objects.get(participant_number="734")
+
+        self.assertEqual(callum_runner_object.participant_number, "811",
+                         "Callum's number is incorrect")
+        self.assertEqual(andrew_runner_object.participant_number, "727",
+                         "Andrew's number is incorrect")
+        self.assertEqual(richard_runner_object.participant_number, "734",
+                         "Richard's number is incorrect")
